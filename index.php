f16dd2ee6350a97e51b7edf1d63d1d10bc50049f
Seongbum Seo <sbum.seo@gmail.com>
2020-09-16 21:36:02 +0900

    Update

--- index.php
+++ index.php
@@ -6,21 +6,43 @@ $mysqli = mysqli_connect($config['host'], $config['user'], $config['password'], 
 <html>
   <head>
     <title>판도라큐브 체크인</title>
+    <style>
+      .message {
+        display: none;
+      }
+    </style>
   </head>
   <body>
     <h1>판도라큐브 체크인</h1>
-    <div class="">
+
+    <form id="form">
       <label for="name">이름</label>
       <input id="name" name="name" type="text" />
       <label for="tell">전화번호</label>
       <input id="tell" name="tell" type="tell" />
       <button id="submit">확인</button>
+    </form>
+
+    <div id="message" class="message">
+      <div class="message-result"></div>
+      <button id="close-button" class="close-button">닫기</button>
     </div>
 
     <script src="jquery.js"></script>
     <script>
       $('#submit').click(function() {
-        console.log('test');
+        $.ajax({
+          type: 'post',
+          url: 'submit.php',
+          cache: false,
+          contentType: false,
+          processData: false,
+          data: new FormData($('#form')[0])
+        }).done(function(result) {
+
+        }).fail(function() {
+
+        });
       });
     </script>
   </body>
--- submit.php
+++ submit.php
@@ -7,5 +7,10 @@ $tell = $_POST['tell'];
 $ip = $_SERVER['REMOTE_ADDR'];
 
 $stmt = $mysqli->prepare('INSERT INTO entrance_log (Name, Tell, IP) VALUES (?, ?, ?)');
-
+$stmt->bind_param('sss', $name, $tell, $ip);
+$result = $stmt->execute();
+if ($result)
+  print('1');
+else
+  print('0');
 ?>
