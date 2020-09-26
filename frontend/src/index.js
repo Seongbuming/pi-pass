3810cc571895ec9eb03ab88905d964aca0b9c9b0
Seongbum Seo <sbum.seo@gmail.com>
2020-09-26 11:32:06 +0900

    Update

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { makeStyles } from '@material-ui/core/styles';
+import { TextField, Button, Icon } from '@material-ui/core';
+import Form from './components/Form';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const useStyles = makeStyles((theme) => ({
+  button: {
+    margin: theme.spacing(1),
+  },
+}));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+function Application(props) {
+  const classes = useStyles();
+  
+  return (
+    <div>
+      <Form>
+        <div>
+          <TextField label="이름" />
+        </div>
+        <div>
+          <TextField label="전화번호" />
+        </div>
+        <div>
+          <Button
+            variant="contained"
+            color="primary"
+            className={classes.button}
+            endIcon={<Icon>send</Icon>}
+          >
+            <span>등록</span>
+          </Button>
+        </div>
+      </Form>
+    </div>
+  );
+}
+
+ReactDOM.render(<Application />, document.getElementById('app'));
