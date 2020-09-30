9b24084f808fd230611f7ebc888cd68b4b27e2fe
Seongbum Seo <sbum.seo@gmail.com>
2020-09-30 12:13:57 +0900

    Update backend

--- backend/app.js
+++ backend/app.js
@@ -1,26 +1,40 @@
 var createError = require('http-errors');
 var express = require('express');
+var cors = require('cors');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var mysql = require('mysql');
+var dbConfig = require('./config.js');
 
 var app = express();
+var database = mysql.createConnection(dbConfig);
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.get('/', (request, response) => {
+  res.send('running');
+});
+
+app.post('/submit', (request, response) => {
+  console.log('');
+  console.log([request.headers['x-forwarded-for'], request.connection.remoteAddress]);
+  console.log(request.body);
+  database.query('INSERT INTO checkin_log SET ?', {
+    Name: request.body.name,
+    Tell: request.body.tell,
+    IP: request.headers['x-forwarded-for'] || request.connection.remoteAddress
+  }, (error, results, fields) => {
+    if (error) throw error;
+  });
+
+  response.send('1');
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
